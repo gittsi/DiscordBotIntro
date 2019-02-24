fba3d294d5250512bfe9a8d25b08b1a643c34e6e
Panagiotis Panagiotopoulos <tsitas@hotmail.co.uk>
2019-02-24 13:11:17 +0200

    Add project files.

---
+++ DiscordBotIntro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscordBotIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
